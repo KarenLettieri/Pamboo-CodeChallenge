502125d50bda043a6823538bc32249853fb7e629
Karen Lettieri <karulettieri2109@gmail.com>
2023-05-06 13:15:13 -0300

    first commit + creation of first components

---
+++ code-challenge/src/components/TaskForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <form>
+        <input type='text' className='' placeholder='Que vamos a hacer hoy?'></input>
+        <button type='submit' className='' ></button>
+    </form>
+  )
+}
+
+export default TaskForm
